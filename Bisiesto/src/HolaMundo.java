dc913b8136079f0ba25d31a60262e955d20886c6
Charkitos03 <103187543+Charkitos03@users.noreply.github.com>
2022-04-20 11:56:02 +0200

    Update HolaMundo.java

--- Bisiesto/src/HolaMundo.java
+++ Bisiesto/src/HolaMundo.java
@@ -1,5 +1,7 @@
 public class HolaMundo {
     public static void main(String[] args) {
         System.out.println("Hola mundo");
+        System.out.println("Hola mundo soy Marcos1");
+        System.out.println("Hola mundo soy Marcos2");
     }
 }
